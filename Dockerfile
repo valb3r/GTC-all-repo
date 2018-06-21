5547fee957a0f68d59d83d9489e096f1d7841bec
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-06-21 14:13:55 +0300

    Added deploy-all docker image

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM openjdk:8
+VOLUME /tmp
+RUN mkdir -p /var/app
+WORKDIR /var/app
+
+ADD all.sh all.sh
+ADD provider/build/libs/provider-1.0-SNAPSHOT.jar provider.jar
+ADD gateway/build/libs/gateway-1.0-SNAPSHOT.jar gateway.jar
+ADD xo-opportunity-trader/build/libs/xo-opportunity-trader-1.0-SNAPSHOT.jar opportunity-trader.jar
+
+RUN bash -c 'touch /var/app/provider.jar'
+RUN bash -c 'touch /var/app/gateway.jar'
+RUN bash -c 'touch /var/app/opportunity-trader.jar'
+
+RUN cd /var/app \
+    && wget https://download.newrelic.com/newrelic/java-agent/newrelic-agent/current/newrelic-java.zip \
+     && unzip newrelic-java.zip && rm newrelic-java.zip
+
+CMD ./all.sh
+
+EXPOSE 8080
+EXPOSE 8084
---
+++ all.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+java \
+    -javaagent:/var/app/newrelic/newrelic.jar \
+    -Dnewrelic.config.agent_enabled="${ENABLE_NEWRELIC:-false}" \
+    -Dnewrelic.config.environment=Prod -Dnewrelic.config.app_name=BookProvider \
+    -Xms64m -Xmx128m -Djava.security.egd=file:/dev/./urandom \
+    -Dfile.encoding=UTF8 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15005 \
+    -jar provider.jar &
+
+java \
+    -javaagent:/var/app/newrelic/newrelic.jar \
+    -Dnewrelic.config.agent_enabled="${ENABLE_NEWRELIC:-false}" \
+    -Dnewrelic.config.environment=Prod -Dnewrelic.config.app_name=TradeGateway \
+    -Xms64m -Xmx128m -Djava.security.egd=file:/dev/./urandom \
+    -Dfile.encoding=UTF8 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15006 \
+    -jar gateway.jar &
+
+java \
+    -javaagent:/var/app/newrelic/newrelic.jar \
+    -Dnewrelic.config.agent_enabled="${ENABLE_NEWRELIC:-false}" \
+    -Dnewrelic.config.environment=Prod -Dnewrelic.config.app_name=XoOpportunityTrader \
+    -Xms128m -Xmx256m -Djava.security.egd=file:/dev/./urandom \
+    -Dfile.encoding=UTF8 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15007 \
+    -jar opportunity-trader.jar &
+
+wait
