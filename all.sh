7b859d76a72452dde96940f091bf309a5e8957ce
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-07-20 18:26:03 +0300

    Reduce memory usage

--- all.sh
+++ all.sh
@@ -4,7 +4,7 @@ java \
     -javaagent:/var/app/newrelic/newrelic.jar \
     -Dnewrelic.config.agent_enabled="${ENABLE_NEWRELIC:-false}" \
     -Dnewrelic.config.environment=Prod -Dnewrelic.config.app_name=BookProvider \
-    -Xms64m -Xmx128m -XX:-TieredCompilation -Xss256k -XX:+UseStringDeduplication \
+    -Xms64m -Xmx128m -XX:-TieredCompilation -Xss256k -XX:+UseStringDeduplication -XX:+UseG1GC \
     -Djava.security.egd=file:/dev/./urandom \
     -Dfile.encoding=UTF8 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15005 \
     -jar provider.jar &
@@ -13,7 +13,7 @@ java \
     -javaagent:/var/app/newrelic/newrelic.jar \
     -Dnewrelic.config.agent_enabled="${ENABLE_NEWRELIC:-false}" \
     -Dnewrelic.config.environment=Prod -Dnewrelic.config.app_name=TradeGateway \
-    -Xms32m -Xmx64m -XX:-TieredCompilation -Xss256k -XX:+UseStringDeduplication \
+    -Xms32m -Xmx64m -XX:-TieredCompilation -Xss256k -XX:+UseStringDeduplication -XX:+UseG1GC \
     -Djava.security.egd=file:/dev/./urandom \
     -Dfile.encoding=UTF8 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15006 \
     -jar gateway.jar &
@@ -22,7 +22,7 @@ java \
     -javaagent:/var/app/newrelic/newrelic.jar \
     -Dnewrelic.config.agent_enabled="${ENABLE_NEWRELIC:-false}" \
     -Dnewrelic.config.environment=Prod -Dnewrelic.config.app_name=XoOpportunityTrader \
-    -Xms128m -Xmx256m -XX:-TieredCompilation -Xss256k -XX:+UseG1GC -XX:+UseStringDeduplication \
+    -Xms128m -Xmx256m -XX:-TieredCompilation -Xss256k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+UseG1GC \
     -Djava.security.egd=file:/dev/./urandom \
     -Dfile.encoding=UTF8 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15007 \
     -jar opportunity-trader.jar &
@@ -31,7 +31,7 @@ java \
     -javaagent:/var/app/newrelic/newrelic.jar \
     -Dnewrelic.config.agent_enabled="${ENABLE_NEWRELIC:-false}" \
     -Dnewrelic.config.environment=Prod -Dnewrelic.config.app_name=Persistor \
-    -Xms32m -Xmx64m -XX:-TieredCompilation -Xss256k -XX:+UseG1GC -XX:+UseStringDeduplication \
+    -Xms32m -Xmx64m -XX:-TieredCompilation -Xss256k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+UseG1GC \
     -Djava.security.egd=file:/dev/./urandom \
     -Dfile.encoding=UTF8 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=15008 \
     -jar persistor.jar &
