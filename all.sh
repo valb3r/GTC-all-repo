b4ea6b5cafec06993cd8fd642141363830eeff6a
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-07-22 19:44:46 +0300

    More mem to persistor

--- all.sh
+++ all.sh
@@ -36,7 +36,7 @@ java \
     -javaagent:newrelic/newrelic.jar \
     -Dnewrelic.config.agent_enabled="${ENABLE_NEWRELIC:-false}" \
     -Dnewrelic.config.environment=Prod -Dnewrelic.config.app_name=Persistor \
-    -Xms32m -Xmx64m -XX:-TieredCompilation -Xss256k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+UseG1GC \
+    -Xms64m -Xmx128m -XX:-TieredCompilation -Xss256k -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+UseG1GC \
     -Djava.security.egd=file:/dev/./urandom \
     -Dfile.encoding=UTF8 \
     -jar persistor.jar &
