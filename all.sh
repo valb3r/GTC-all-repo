d1190b962de028621a43113b72c9ffd94cbea6b9
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-07-23 11:51:14 +0300

    No tiered compilation and do use G1GC for gateway

--- all.sh
+++ all.sh
@@ -18,7 +18,7 @@ java \
     -javaagent:newrelic/newrelic.jar \
     -Dnewrelic.config.agent_enabled="${ENABLE_NEWRELIC:-false}" \
     -Dnewrelic.config.environment=Prod -Dnewrelic.config.app_name=TradeGateway \
-    -Xms32m -Xmx64m -Xss256k \
+    -Xms32m -Xmx64m -XX:-TieredCompilation -Xss256k -XX:+UseStringDeduplication -XX:+UseG1GC \
     -Djava.security.egd=file:/dev/./urandom \
     -Dfile.encoding=UTF8 \
     -jar gateway.jar &
