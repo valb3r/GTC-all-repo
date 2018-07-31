8793df531fe4ea307f0363d342704312b125f2b4
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-07-31 12:28:04 +0300

    Fixed persistor

--- persistor/src/main/java/com/gtc/persistor/service/BookPersistor.java
+++ persistor/src/main/java/com/gtc/persistor/service/BookPersistor.java
@@ -122,8 +122,8 @@ public class BookPersistor {
         file.write("Time\t");
         file.write("Best buy\t");
         file.write("Best sell\t");
-        file.write("Histogram price buy step\t");
-        file.write("Histogram price sell step\t");
+        file.write("Histogram price Buy step\t");
+        file.write("Histogram price Sell step\t");
 
         writeAmounts(file, "Buy amount at ", histogramBuyPrecision);
         writeAmounts(file, "Sell amount at ", histogramSellPrecision);
