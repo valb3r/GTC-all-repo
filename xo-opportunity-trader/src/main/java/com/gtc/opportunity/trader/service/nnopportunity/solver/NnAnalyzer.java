69e2c14bec08ab19b4d1f437f0eae128792633c5
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-08-10 13:37:19 +0300

    Ignore rejected exception

--- xo-opportunity-trader/src/main/java/com/gtc/opportunity/trader/service/nnopportunity/solver/NnAnalyzer.java
+++ xo-opportunity-trader/src/main/java/com/gtc/opportunity/trader/service/nnopportunity/solver/NnAnalyzer.java
@@ -2,13 +2,16 @@ package com.gtc.opportunity.trader.service.nnopportunity.solver;
 
 import com.gtc.model.provider.OrderBook;
 import com.gtc.opportunity.trader.service.nnopportunity.creation.NnCreateTradesService;
+import com.gtc.opportunity.trader.service.xoopportunity.creation.fastexception.RejectionException;
 import com.newrelic.api.agent.Trace;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by Valentyn Berezin on 29.07.18.
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class NnAnalyzer {
@@ -26,6 +29,8 @@ public class NnAnalyzer {
                 orderBook.getMeta().getPair().getTo().getCode()));
         try {
             solver.findStrategy(orderBook).ifPresent(strategy -> createTradesService.create(strategy, orderBook));
+        } catch (RejectionException ex) {
+            log.info("Rejected {}", ex);
         } finally {
             Thread.currentThread().setName(oldName);
         }
