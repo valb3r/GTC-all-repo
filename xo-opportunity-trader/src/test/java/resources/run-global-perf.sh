b0141190207372140a9e445ee74eda2a8c6cdf52
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-08-10 17:09:27 +0300

    Global-perf test template

---
+++ xo-opportunity-trader/src/test/java/resources/run-global-perf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+max_gain=5
+
+export GLOBAL_NN_TEST="true"
+export START="2018-07-27T00:00:00"
+export END="2018-07-28T23:59:59"
+
+for gain in $(seq 1 $max_gain);
+do
+	for level in $(seq 1 $max_gain);
+	do
+		export FUTURE_GAIN_PCT="0.$gain"
+		export NOOP_THRESHOLD="1.00$level"
+		echo "for gain $FUTURE_GAIN_PCT / level $NOOP_THRESHOLD"
+		cd ../../../../..
+		./gradlew clean :xo-opportunity-trader:test --tests com.gtc.opportunity.trader.service.nnopportunity.global.GlobalNnPerformanceTest.test &> "/home/valb3r/Desktop/log/g"$gain"_l"$level".log"
+	done
+done
