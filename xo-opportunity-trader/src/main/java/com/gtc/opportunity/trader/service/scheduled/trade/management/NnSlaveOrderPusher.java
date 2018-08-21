d0d92d275299bf9ef5eb6e1d1c413ebfed5da564
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-08-21 19:56:17 +0300

    Do not create slave order if master is done, but it is too old

--- xo-opportunity-trader/src/main/java/com/gtc/opportunity/trader/service/scheduled/trade/management/NnSlaveOrderPusher.java
+++ xo-opportunity-trader/src/main/java/com/gtc/opportunity/trader/service/scheduled/trade/management/NnSlaveOrderPusher.java
@@ -2,12 +2,15 @@ package com.gtc.opportunity.trader.service.scheduled.trade.management;
 
 import com.gtc.opportunity.trader.domain.*;
 import com.gtc.opportunity.trader.repository.TradeRepository;
+import com.gtc.opportunity.trader.service.CurrentTimestamp;
 import com.gtc.opportunity.trader.service.statemachine.nnaccept.NnDependencyHandler;
+import com.gtc.opportunity.trader.service.xoopportunity.creation.ConfigCache;
 import com.newrelic.api.agent.Trace;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.service.StateMachineService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,8 +28,11 @@ import static com.gtc.opportunity.trader.domain.Const.InternalMessaging.ORDER_ID
 @RequiredArgsConstructor
 public class NnSlaveOrderPusher {
 
+    private final CurrentTimestamp timestamp;
+    private final ConfigCache config;
     private final NnDependencyHandler handler;
     private final TradeRepository repository;
+    private final StateMachineService<TradeStatus, TradeEvent> tradeMachines;
     private final StateMachineService<NnAcceptStatus, AcceptEvent> nnMachineSvc;
 
     @Trace(dispatcher = true) // tracing since it is very important part
@@ -43,6 +49,10 @@ public class NnSlaveOrderPusher {
         String oldName = Thread.currentThread().getName();
         Thread.currentThread().setName("Push dependent " + trade.getId());
         try {
+            if (checkForExpirationAndExpireIfNeeded(trade)) {
+                return;
+            }
+
             if (!handler.publishDependentOrderIfPossible(trade)) {
                 return;
             }
@@ -59,5 +69,23 @@ public class NnSlaveOrderPusher {
         } finally {
             Thread.currentThread().setName(oldName);
         }
+    }
+
+    private boolean checkForExpirationAndExpireIfNeeded(Trade trade) {
+        NnConfig cfg = config.getNnCfg(
+                trade.getClient().getName(),
+                trade.getCurrencyFrom(),
+                trade.getCurrencyTo())
+                .orElseThrow(() -> new IllegalStateException("No config"));
+
+        if (trade.getRecordedOn().plusMinutes(cfg.getMaxSlaveDelayM()).compareTo(timestamp.dbNow()) > 0) {
+            return false;
+        }
+
+        log.info("Trade {} is expired! - sending cancel signal", trade.getId());
+        StateMachine<TradeStatus, TradeEvent> machine = tradeMachines.acquireStateMachine(trade.getId());
+        machine.sendEvent(TradeEvent.CANCELLED);
+        tradeMachines.releaseStateMachine(machine.getId());
+        return true;
     }
 }
