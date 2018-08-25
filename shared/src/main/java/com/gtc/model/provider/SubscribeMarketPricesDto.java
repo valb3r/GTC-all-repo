1e3d10b1990cc14ace1d314a05f00c3a3be65dc3
Valentyn Berezin <valentyn.berezin@aurea.com>
2018-08-25 21:20:19 +0300

    Restored lost work

--- shared/src/main/java/com/gtc/model/provider/SubscribeMarketPricesDto.java
+++ shared/src/main/java/com/gtc/model/provider/SubscribeMarketPricesDto.java
@@ -1,10 +1,8 @@
 package com.gtc.model.provider;
 
 import com.gtc.meta.TradingCurrency;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
@@ -12,11 +10,15 @@ import java.util.Set;
  * Created by Valentyn Berezin on 25.08.18.
  */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 public class SubscribeMarketPricesDto extends ProviderSubsDto {
 
     private TradingCurrency from;
     private Set<TradingCurrency> to;
+
+    public SubscribeMarketPricesDto(TradingCurrency from, Set<TradingCurrency> to) {
+        super(Mode.MARKET_PRICE);
+        this.from = from;
+        this.to = to;
+    }
 }
